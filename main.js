ab881bd58635285f2625757265564f900f1eb49e
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-01-21 05:53:52 -0600

    myfirstcommit;

---
+++ main.js
@@ -0,0 +1,8 @@
+function revealMessage (){
+    document.getElementById('hiddenMessage').style.display= 'block';
+}
+function countDown (){
+    var currentVal = document.getElementById('countDownButton').innerHTML;
+    var newVal = currentVal-1;
+    document.getElementById('countDownButton').innerHTML=newVal;
+}
